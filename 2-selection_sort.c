bca05417a0931ae2474fe8f339e725bd0a0c1239
elvisthomme <elvisthomme@gmail.com>
2022-07-18 12:17:51 +0100

    Selection sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,29 @@
+#include "sort.h"
+/**
+  * selection_sort - sort an array using selection sort
+  *
+  * @array: the array to sort
+  * @size: the size of the array
+  */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, s;
+	int tmp;
+
+	if (array)
+	for(i = 0; i < size - 1; i++)
+	{
+		s = i;
+		for (j = s + 1; j < size; j++)
+			if (*(array + j) < *(array + s))
+				s = j;
+		if (s != i)
+		{
+			tmp = *(array + i);
+			*(array + i) = *(array + s);
+			*(array + s) = tmp;
+			print_array(array, size);
+
+		}
+	}
+}
